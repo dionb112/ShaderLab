d032a94dc325ff191f47b760efe6f3f6aabec678
Dion Buckley <dion-b@hotmail.com>
2018-02-18 12:07:40 +0000

    Lab 10 project setup ready to work

---
+++ SFMLOpenGL/Game.h
@@ -0,0 +1,38 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <Debug.h>
+
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/wglew.h>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+
+#include <MyVector3D.h>
+#include <MyMatrix3.h>
+
+using namespace std;
+using namespace sf;
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	void run();
+private:
+	Window window;
+	bool isRunning = false;
+	void initialize();
+	void update();
+	void render();
+	void unload();
+
+	Clock clock;
+	Time elapsed;
+
+	float rotationAngle = 0.0f;
+};
+
+#endif
